58466b8f0257f29c6f01292a26a47a72592ad3cc
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-04-30 15:28:16 +0200

    added TouristInfoCards.js

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">
+        CYF Hotel
+        <img src="https://image.flaticon.com/icons/svg/139/139899.svg" />
+      </header>
+    </div>
+  );
+};
+
+export default Heading;
